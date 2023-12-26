832b1e765c0c47e6f1bf3d10fa935b0478746058
ekkang0820 <104552382+ekkang0820@users.noreply.github.com>
2023-12-26 17:30:31 +0900

    commit

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+
+# ABC더하기
 # input_str = input()
 # input_list = input_str.split()
 # A = [int(x) for x in input_list]
@@ -7,7 +9,7 @@
 #
 # print(temp)
 
-
+#4바이트마다 long 붙혀 문저열 완성하기
 # longinput = int(input())
 # count = int(longinput/4)
 # longstr = "long "
@@ -17,6 +19,10 @@
 #
 # print(longstr)
 #
+
+
+
+#마름모 별찍기
 # cnt = int(input())
 # cntstar = 0
 # cntblank = cnt
@@ -44,6 +50,9 @@
 #
 # print(str)
 
+
+
+#기러기 인도인 토마토 우영우
 # str = list(input())
 # restr = list(reversed(str))
 # lencnt = int(len(str)/2)
@@ -55,6 +64,9 @@
 #
 # print(result)
 
+
+
+#가장 많이 사용한 알파벳
 strInput = list(input().upper())
 cntstr = {}
 cnt = 0
